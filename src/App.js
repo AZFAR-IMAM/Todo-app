ae67af83fb1307d09a3a22e640d56e98e9c55fa7
Azfar Imam <azfarimam785@gmail.com>
2024-02-25 11:17:34 +0530

    Edit feature added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import { useState } from "react";
 import "./App.css";
+import { AiFillDelete } from "react-icons/ai";
+import { MdEditNote } from "react-icons/md";
+import { FaArrowRight } from "react-icons/fa";
+
 function App() {
   const [item, setItem] = useState("");
   const [todo, setTodo] = useState([]);
+  const [editId, setEditId] = useState(0);
+  const handelEdit = (id) => {
+    const editTodo = todo.find((x) => x.id === id);
+    setItem(editTodo.item);
+    setEditId(id);
+  };
   const handelSubmit = (e) => {
+    if (editId) {
+      const editTodo = todo.find((x) => x.id === editId.id);
+      const updatedTodo = todo.map((t) =>
+        t.id === editTodo
+          ? (t = { id: t.id, todo })
+          : { id: t.id, item: t.item }
+      );
+      setTodo(updatedTodo);
+      setItem("");
+      setEditId(0);
+    }
     e.preventDefault();
     if (item !== "") {
       setTodo([{ id: `${item}-${Date.now()}`, item }, ...todo]);
@@ -14,6 +35,8 @@ function App() {
     const delTodos = todo.filter((ele) => ele.id !== id);
     setTodo([...delTodos]);
   };
+
+  console.log("here", editId);
   return (
     <div className="app">
       <div className="container">
@@ -25,7 +48,7 @@ function App() {
             value={item}
             onChange={(e) => setItem(e.target.value)}
           />
-          <button>add</button>
+          <button>{editId ? "edit" : <FaArrowRight />}</button>
         </form>
         <ul className="allTodos">
           {todo.map((itemList) => {
@@ -34,8 +57,12 @@ function App() {
                 <span className="todoText" key={itemList.id}>
                   {itemList.item}
                 </span>
-                <button>edit</button>
-                <button onClick={() => handelDel(itemList.id)}>delete</button>
+                <button onClick={() => handelEdit(itemList.id)}>
+                  <MdEditNote />
+                </button>
+                <button onClick={() => handelDel(itemList.id)}>
+                  <AiFillDelete />
+                </button>
               </li>
             );
           })}
